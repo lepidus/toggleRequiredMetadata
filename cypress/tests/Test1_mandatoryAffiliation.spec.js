d7ba80b19eecc954b812e990785c130f5beb1488
Jhon <jhon@lepidus.com.br>
2022-02-08 13:47:36 -0400

    Adds integration test for affiliation field when adding contributor
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#265
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>
    Signed-off-by: Lucas Anselmo <lucasanselmo@lepidus.com.br>

--- cypress/tests/Test1_mandatoryAffiliation.spec.js
+++ cypress/tests/Test1_mandatoryAffiliation.spec.js
@@ -13,10 +13,11 @@ describe('Mandatory Metadata Plugin - Affiliation is mandatory', function() {
         loginAdminUser();
         cy.get("#active-button").click();
         cy.get(".listPanel__item:visible > .listPanel__item--submission > .listPanel__itemSummary > .listPanel__itemActions > a").first().click();
+        cy.wait(2000);
         cy.get("#publication-button").click();
         cy.get("#contributors-button").click();
         cy.get("#contributors-grid > .pkp_controllers_grid > .header > .actions > li:last-of-type > a").click();
-        //cy.get("input[id^=affiliation-]:visible").should('have.attr', 'required', 'true');
+        cy.get("input[id^=affiliation-]").should('have.attr', 'required');
     });
 
 });
