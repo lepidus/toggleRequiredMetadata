6731bd8896d74f2fc373d37eb87228cfa165f25a
Iudi Zurba Melgarejo <iudi@lepidus.com.br>
2022-08-30 18:03:59 -0300

    Format PHP to PSR-12
    Signed-off-by: iudi <iudi@lepidus.com>
    Signed-off-by: vitoria <vitoria@lepidus.com>

--- ToggleRequiredMetadataPlugin.inc.php
+++ ToggleRequiredMetadataPlugin.inc.php
@@ -12,35 +12,42 @@
 
 import('lib.pkp.classes.plugins.GenericPlugin');
 
-class ToggleRequiredMetadataPlugin extends GenericPlugin {
+class ToggleRequiredMetadataPlugin extends GenericPlugin
+{
+    public function register($category, $path, $mainContextId = null)
+    {
+        $success = parent::register($category, $path, $mainContextId);
 
-	public function register($category, $path, $mainContextId = NULL) {
-		$success = parent::register($category, $path, $mainContextId);
-		
-		if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) return true;
-		if ($success && $this->getEnabled($mainContextId)) {
-			HookRegistry::register('authorform::display', array($this, 'editAuthorFormTemplate'));
-		}
-		return $success;
-	}
+        if (!Config::getVar('general', 'installed') || defined('RUNNING_UPGRADE')) {
+            return true;
+        }
+        if ($success && $this->getEnabled($mainContextId)) {
+            HookRegistry::register('authorform::display', array($this, 'editAuthorFormTemplate'));
+        }
+        return $success;
+    }
 
-	public function getDisplayName() {
-		return __('plugins.generic.toggleRequiredMetadata.displayName');
-	}
+    public function getDisplayName()
+    {
+        return __('plugins.generic.toggleRequiredMetadata.displayName');
+    }
 
-	public function getDescription() {
-		return __('plugins.generic.toggleRequiredMetadata.description');
-	}
+    public function getDescription()
+    {
+        return __('plugins.generic.toggleRequiredMetadata.description');
+    }
 
-	public function editAuthorFormTemplate($hookName, $params) {
-		$request = PKPApplication::get()->getRequest();
-		$templateMgr = TemplateManager::getManager($request);
-		$templateMgr->registerFilter("output", array($this, 'authorAffiliationFormFilter'));
-		
-		return false;
-	}
+    public function editAuthorFormTemplate($hookName, $params)
+    {
+        $request = PKPApplication::get()->getRequest();
+        $templateMgr = TemplateManager::getManager($request);
+        $templateMgr->registerFilter("output", array($this, 'authorAffiliationFormFilter'));
 
-	public function authorAffiliationFormFilter($output, $templateMgr) {
+        return false;
+    }
+
+    public function authorAffiliationFormFilter($output, $templateMgr)
+    {
         $selectAffiliationInput = '/<input[^>]+id="affiliation[^>]*>/';
         if (preg_match_all($selectAffiliationInput, $output, $matches, PREG_OFFSET_CAPTURE)) {
             $output = $this->setRequiredOnInputFields($output, $matches);
@@ -50,34 +57,35 @@ class ToggleRequiredMetadataPlugin extends GenericPlugin {
         return $output;
     }
 
-	private function setRequiredOnInputFields($output, $inputFieldMatches) {
-		$timesEditedOutput = 0;
-		foreach($inputFieldMatches[0] as $match) {
-			$matchedText = $match[0];
-			$posMatch = $match[1];
-			
-			$requiredParam = " required=\"true\" ";
-			$inputTagStart = "<input " ;
-			$editionsOffset = $timesEditedOutput*strlen($requiredParam);
-			
-			$output = substr_replace($output, $requiredParam, $posMatch + strlen($inputTagStart) + $editionsOffset, 0);
-			$timesEditedOutput++;
-		}
+    private function setRequiredOnInputFields($output, $inputFieldMatches)
+    {
+        $timesEditedOutput = 0;
+        foreach ($inputFieldMatches[0] as $match) {
+            $matchedText = $match[0];
+            $posMatch = $match[1];
 
-		return $output;
-	}
+            $requiredParam = " required=\"true\" ";
+            $inputTagStart = "<input " ;
+            $editionsOffset = $timesEditedOutput * strlen($requiredParam);
 
-	private function addRequiredFieldSpanToLabel($output) {
-		$selectAffiliationLabelOpening = "/<label *class=\"sub_label\" *for=\"affiliation[^>]+>/";
-		preg_match($selectAffiliationLabelOpening, $output, $matches, PREG_OFFSET_CAPTURE);
-		$posStartContentLabel = $matches[0][1] + strlen($matches[0][0]);
+            $output = substr_replace($output, $requiredParam, $posMatch + strlen($inputTagStart) + $editionsOffset, 0);
+            $timesEditedOutput++;
+        }
 
-		$selectAffiliationLabelEnding = '/<\/label>/';
-		preg_match($selectAffiliationLabelEnding, $output, $matches, PREG_OFFSET_CAPTURE, $posStartContentLabel);
-		$posEndContentLabel = $matches[0][1];
+        return $output;
+    }
 
-		$requiredFieldSpan = "<span class=\"req\">*</span> ";
-		return substr_replace($output, $requiredFieldSpan, $posEndContentLabel, 0);
-	}
+    private function addRequiredFieldSpanToLabel($output)
+    {
+        $selectAffiliationLabelOpening = "/<label *class=\"sub_label\" *for=\"affiliation[^>]+>/";
+        preg_match($selectAffiliationLabelOpening, $output, $matches, PREG_OFFSET_CAPTURE);
+        $posStartContentLabel = $matches[0][1] + strlen($matches[0][0]);
 
+        $selectAffiliationLabelEnding = '/<\/label>/';
+        preg_match($selectAffiliationLabelEnding, $output, $matches, PREG_OFFSET_CAPTURE, $posStartContentLabel);
+        $posEndContentLabel = $matches[0][1];
+
+        $requiredFieldSpan = "<span class=\"req\">*</span> ";
+        return substr_replace($output, $requiredFieldSpan, $posEndContentLabel, 0);
+    }
 }
