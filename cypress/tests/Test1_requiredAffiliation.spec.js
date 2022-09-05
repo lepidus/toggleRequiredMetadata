3122a1227717bfc5426afef02c0ca8e9f640235d
vitoria <vitoria@lepidus.com.br>
2022-09-05 16:35:28 -0300

    refactor cypress code
    issue: documentacao-e-tarefas/desenvolvimento_e_infra#409
    
    Signed-off-by: vitoria <vitoria@lepidus.com.br>
    Signed-off-by: iudi <iudi@lepidus.com.br>

--- cypress/tests/Test1_requiredAffiliation.spec.js
+++ cypress/tests/Test1_requiredAffiliation.spec.js
@@ -6,29 +6,8 @@ function loginAdminUser() {
     cy.get('button[class=submit]').click();
 }
 
-// describe('Required Metadata Plugin - Affiliation and ORCID is Required', function() {
-//     it('Check if affiliation and orcid is required at edit author form', function() {
-        
-//         cy.visit(Cypress.env('baseUrl') + 'index.php/anphlac/submissions');
-//         loginAdminUser();
-        
-//         cy.get(':nth-child(1) > :nth-child(1) > .app__navItem').click();
-//         cy.get("#active-button").click();
-//         cy.get(".listPanel__item:visible > .listPanel__item--submission > .listPanel__itemSummary > .listPanel__itemActions > a").first().click();
-//         cy.wait(2000);
-//         cy.get("#publication-button").click();
-//         cy.get("#contributors-button").click();
-//         cy.get("#contributors-grid > .pkp_controllers_grid > .header > .actions > li:last-of-type > a").click();
-//         cy.get("input[id^=affiliation-]").should('have.attr', 'required');
-//         cy.get("input[id^=orcid-]").should('have.attr', 'required');
-//     });
-
-// });
-
-
 describe('Required Metadata Plugin - Make Affiliation and ORCID Required', function() {
     it('Check if affiliation and orcid is required at edit author form', function() {
-        
         cy.visit(Cypress.env('baseUrl') + 'index.php/anphlac/submissions');
         loginAdminUser();
         
@@ -51,7 +30,6 @@ describe('Required Metadata Plugin - Make Affiliation and ORCID Required', funct
         cy.get("#contributors-grid > .pkp_controllers_grid > .header > .actions > li:last-of-type > a").click();
         cy.get("input[id^=affiliation-]").should('have.attr', 'required');
         cy.get("input[id^=orcid-]").should('have.attr', 'required');
-        
     });
 
 });
