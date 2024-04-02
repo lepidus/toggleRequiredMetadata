e7640134ca026cbc42b97a6b263933ddb024e24d
Jhon <jhon@lepidus.com.br>
2024-04-02 15:54:19 -0400

    Adds class for metadata checking
    Issue: documentacao-e-tarefas/desenvolvimento_e_infra#790
    
    Signed-off-by: Jhon <jhon@lepidus.com.br>

---
+++ classes/MetadataChecker.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace APP\plugins\generic\toggleRequiredMetadata\classes;
+
+class MetadataChecker
+{
+    private function checkRequiredMetadata(array $authors, string $metadata): bool
+    {
+        foreach ($authors as $author) {
+            if (!$author->getData($metadata)) {
+                return false;
+            } elseif (is_array($author->getData($metadata))) {
+                $entryFilled = false;
+                foreach ($author->getData($metadata) as $entry) {
+                    if ($entry) {
+                        $entryFilled = true;
+                        break;
+                    }
+                }
+                if (!$entryFilled) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public function checkOrcids(array $authors): bool
+    {
+        return $this->checkRequiredMetadata($authors, 'orcid');
+    }
+
+    public function checkAffiliations(array $authors): bool
+    {
+        return $this->checkRequiredMetadata($authors, 'affiliation');
+    }
+
+    public function checkBiographies(array $authors): bool
+    {
+        return $this->checkRequiredMetadata($authors, 'biography');
+    }
+}
